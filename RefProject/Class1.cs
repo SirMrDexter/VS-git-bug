d1ddfa1ae1e16459f4e5117965b78b80aedb7ca2
Ramkumar Krishnamoorthy <ramkumar1985@gmail.com>
2022-09-05 17:19:49 +1000

    Initial commit

---
+++ RefProject/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefProject
+{
+    public class MyClass
+    {
+        public static int SayNumber()
+        {
+            return 1;
+        }
+    }
+}
